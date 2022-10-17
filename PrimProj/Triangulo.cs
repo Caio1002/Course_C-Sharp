4830adcd0c0a7fc57840e1c731f6bc24cdaf455c
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-10-16 23:33:07 -0300

    Class Triangulo

--- PrimProj/Triangulo.cs
+++ PrimProj/Triangulo.cs
@@ -13,6 +13,12 @@ namespace PrimProj
         public double B;
         public double C;
 
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
     }
 
 }
