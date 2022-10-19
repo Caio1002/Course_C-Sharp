72fc751d807fb0832bdd2e5cb222cf19c4dd2719
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-10-19 00:31:57 -0300

    Retangulo

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -7,7 +7,7 @@ namespace PrimProj
     {
         static void Main(string[] args)
         {
-
+            /*
             Produto p = new Produto();
 
             Console.WriteLine("Entre com os dados do produto:");
@@ -35,7 +35,21 @@ namespace PrimProj
 
             Console.WriteLine();
             Console.WriteLine("Dados atualizados: " + p);
+            */
 
+            Retangulo ret = new Retangulo();
+            double ar = 0;
+
+            Console.Write("Entre com a largura: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Entre com a altura: ");
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Area: " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + ret.Perim().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + ret.Diag().ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
---
+++ PrimProj/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace PrimProj
+{
+    internal class Retangulo
+    {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perim()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diag()
+        {
+            return Math.Sqrt((Altura * Altura) + (Largura * Largura));
+        }
+
+    }
+}
