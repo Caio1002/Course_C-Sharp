4a2706e6867246ae0bfd7a8aaf5ce20b4737a521
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-10-19 22:58:04 -0300

    Aluno

--- PrimProj/Funcionario.cs
+++ PrimProj/Funcionario.cs
@@ -4,6 +4,28 @@ namespace PrimProj
 {
     internal class Funcionario
     {
+        /*
+         MAIN
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Entre com os dados do funcionário: ");
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(func);
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porc = double.Parse(Console.ReadLine());
+            func.AumentaSalario(porc);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+        */
 
         public string Nome;
         public double Salario;
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -8,27 +8,29 @@ namespace PrimProj
         static void Main(string[] args)
         {
 
-            Funcionario func = new Funcionario();
+            Aluno aluno = new Aluno();
 
-            Console.WriteLine("Entre com os dados do funcionário: ");
-            Console.Write("Nome: ");
-            func.Nome = Console.ReadLine();
-            Console.Write("Salário bruto: ");
-            func.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Imposto: ");
-            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine(func);
+            Console.WriteLine("Nota final = "
+                + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine();
-            Console.Write("Digite a porcentagem para aumentar o salário: ");
-            double porc = double.Parse(Console.ReadLine());
-            func.AumentaSalario(porc);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + func);
-
-
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                    + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                    + " PONTOS");
+            }
         }
     }
 }
