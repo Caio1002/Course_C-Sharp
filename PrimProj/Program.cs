6320a8dc1f7124f14f658ddde014b412c5f11364
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-11-06 00:01:10 -0300

    Retorno!!Encapsulamento

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -8,45 +8,9 @@ namespace PrimProj
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Entre com os dados do produto:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            
+            Console.WriteLine("Hello world!!! Voltando após a conclusão de semestre da faculdade!");
 
-            /*TESTE PARA CONSTRUTOR COM 2 ARGUMENTOS
-            Console.Write("Quantidade em estoque: ");
-            int quantidade = int.Parse(Console.ReadLine());*/
 
-            Produto p = new Produto(nome, preco);
-
-            /*SINTAXE ALTERNATIVA*/
-            Produto p3 = new Produto
-            {
-                Nome = "TV",
-                Preco = 200.99,
-                Quantidade = 20
-            };
-
-            Console.WriteLine("Dados do produto: " + p);
-            Console.WriteLine();
-
-            Console.Write("Digite o numero de produtos a serem adicionados ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o numero de produtos a serem removidos do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            
         }
     }
 }
