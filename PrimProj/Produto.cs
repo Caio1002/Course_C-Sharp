661b8c7e9e05a56bc35440f5190f0fb00dc4d037
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-11-06 00:01:10 -0300

    Encapsulamento

--- PrimProj/Produto.cs
+++ PrimProj/Produto.cs
@@ -38,52 +38,75 @@ namespace PrimProj
             Console.WriteLine();
             Console.WriteLine("Dados atualizados: " + p);
         */
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
-        public Produto()
+        /*
+        Get = obter
+        Set = alterar
+        */
+        public string GetNome()
         {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
         }
 
         public Produto(string nome, double preco, int quantidade)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
         }
 
         /*SOBRECARGA*/
         public Produto(string nome, double preco)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = 0;/*Já inicia com 0, portanto não é necessaria essa linha*/
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 0;/*Já inicia com 0, portanto não é necessaria essa linha*/
         }
 
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade)
         {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString()
         {
             return
-                Nome
+                _nome
                 + ", $ "
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + Quantidade
+                + _quantidade
                 + " unidades, Total: $"
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
@@ -113,6 +136,47 @@ namespace PrimProj
             Console.Write("Digite o numero de produtos a serem removidos do estoque: ");
             qte = int.Parse(Console.ReadLine());
             p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        */
+
+        /*
+        Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+
+            /*TESTE PARA CONSTRUTOR COM 2 ARGUMENTOS
+            Console.Write("Quantidade em estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+        Produto p = new Produto(nome, preco);
+
+        /*SINTAXE ALTERNATIVA
+        Produto p3 = new Produto
+        {
+            Nome = "TV",
+            Preco = 200.99,
+            Quantidade = 20
+        };
+
+        Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o numero de produtos a serem adicionados ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+        p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+        p.RemoverProdutos(qte);
 
             Console.WriteLine();
             Console.WriteLine("Dados atualizados: " + p);
