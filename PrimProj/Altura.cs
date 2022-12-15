2daa9d6e4e4c497979d9a37e382b848196c0e5f1
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-12-14 22:12:38 -0300

    Atividade vetores

---
+++ PrimProj/Altura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimProj {
+    internal class Altura {
+
+        /*
+            MAIN
+            Console.Write("Digite a quantidade de pessoas: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[numero];
+            Console.WriteLine();
+            Console.WriteLine("Digite as alturas:");
+            for(int i = 0; i < numero; i++) {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            double soma = 0;
+            for(int i = 0; i < numero; i++) {
+                soma += vect[i];
+            }
+            double media = soma / numero;
+
+            Console.WriteLine();
+            Console.WriteLine("A média de altura é: " + media.ToString("F2", CultureInfo.InvariantCulture));
+        */
+
+    }
+}
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -5,23 +5,26 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            Console.Write("Digite a quantidade de pessoas: ");
-            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de produtos: ");
+            int n = int.Parse(Console.ReadLine());
 
-            double[] vect = new double[numero];
-            Console.WriteLine();
-            Console.WriteLine("Digite as alturas:");
-            for(int i = 0; i < numero; i++) {
-                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            }
-            double soma = 0;
-            for(int i = 0; i < numero; i++) {
-                soma += vect[i];
-            }
-            double media = soma / numero;
+            Product[] vect = new Product[n];
 
-            Console.WriteLine();
-            Console.WriteLine("A média de altura é: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Nome do produto:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Valor do produto:");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double soma = 0.0;
+            for(int i = 0; i < n; i++) {
+                soma += vect[i].Price;
+            }
+            double media = soma / n;
+            Console.WriteLine("Preço médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
 
         }
     }
