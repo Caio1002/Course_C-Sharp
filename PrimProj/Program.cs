fd71b05d7344ceb985ac95e6a489cd24d5562b6d
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-12-20 22:05:40 -0300

    Params

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -5,33 +5,12 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Quantos quartos serão alugados?");
-            int quant = int.Parse(Console.ReadLine());
-            Console.WriteLine();
+            int s1 = CalcParams.Sum(2, 3);
+            Console.WriteLine(s1);
 
-            Quarto[] quarto = new Quarto[10];
+            int s2 = CalcParams.Sum(2, 3, 4);
+            Console.WriteLine(s2);
 
-            for (int i = 0; i < quant; i++) {
-                Console.WriteLine($"Aluguel #{i+1}:");
-                Console.WriteLine();
-                Console.Write("Nome do titular: ");
-                string name = Console.ReadLine();
-                Console.Write("Email do titular: ");
-                string mail = Console.ReadLine();
-                Console.Write("Numero do quarto: ");
-                int num = int.Parse(Console.ReadLine());
-                Console.WriteLine();
-                quarto[num] = new Quarto { Nome = name, Email = mail };
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Quartos ocupados:");
-            for (int i = 0; i < 10; i++) {
-                if (quarto[i] != null) {
-                    Console.WriteLine(i + ": " + quarto[i]);
-
-                }
-            }
         }
     }
 }
--- PrimProj/Quarto.cs
+++ PrimProj/Quarto.cs
@@ -7,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace PrimProj {
     internal class Quarto {
-        
+
+        /*
+            MAIN
+            Console.WriteLine("Quantos quartos serão alugados?");
+            int quant = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Quarto[] quarto = new Quarto[10];
+
+            for (int i = 0; i < quant; i++) {
+                Console.WriteLine($"Aluguel #{i+1}:");
+                Console.WriteLine();
+                Console.Write("Nome do titular: ");
+                string name = Console.ReadLine();
+                Console.Write("Email do titular: ");
+                string mail = Console.ReadLine();
+                Console.Write("Numero do quarto: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                quarto[num] = new Quarto { Nome = name, Email = mail };
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++) {
+                if (quarto[i] != null) {
+                    Console.WriteLine(i + ": " + quarto[i]);
+
+                }
+            }
+            */
+
         public string Nome { get; set; }
         public string Email { get; set; }
 
