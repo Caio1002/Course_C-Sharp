0e8cd45c1a0aab68280d2c78c4676fc513ba297e
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-12-21 22:29:55 -0300

    Foreach

--- PrimProj/CalcParams.cs
+++ PrimProj/CalcParams.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace PrimProj {
     internal class CalcParams {
 
+        /*
+            MAIN
+            int s1 = CalcParams.Sum(2, 3);
+            Console.WriteLine(s1);
+
+            int s2 = CalcParams.Sum(2, 3, 4);
+            Console.WriteLine(s2);
+            */
+
         public static int Sum(params int[] numbers) {
             int sum = 0;
             for(int i=0; i < numbers.Length; i++) {
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -4,13 +4,11 @@ using System.Globalization;
 namespace PrimProj {
     class Program {
         static void Main(string[] args) {
-
-            int s1 = CalcParams.Sum(2, 3);
-            Console.WriteLine(s1);
-
-            int s2 = CalcParams.Sum(2, 3, 4);
-            Console.WriteLine(s2);
-
+            /*TESTE FOREACH*/
+            string[] vect = new string[] { "Maria", "Pedro", "Ana" };
+            foreach(string obj in vect) {
+                Console.WriteLine(obj);
+            }    
         }
     }
 }
