51689d1077cbddf87e85a2bdedaba9b88eb3576a
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-12-26 20:59:15 -0300

    Empregado

---
+++ PrimProj/Empregado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimProj {
+    class Empregado {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Empregado(int id, string nome, double salario) {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AcrescentarSalario(double porcentagem) {
+            Salario += Salario * porcentagem / 100.0;
+        }
+
+        public override string ToString() {
+            return "ID:"
+                + Id
+                + ", Nome: "
+                + Nome
+                + ", Salario: R$ "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -5,68 +5,43 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace PrimProj {
     class Program {
-            static void Main(string[] args) {
+        static void Main(string[] args) {
 
-                List<string> list = new List<string>();
+            Console.Write("Quantidade de empregados a serem registrados: ");
+            int quant = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
-                list.Add("Maria");
-                list.Add("Alex");
-                list.Add("Bob");
-                list.Add("Ana");
-                list.Add("Carlos");
-                list.Add("João");
-                list.Insert(2, "Marco");
+            List<Empregado> lista = new List<Empregado>();
 
-                foreach (string obj in list) {
-                    Console.WriteLine(obj);
-                }
-                Console.WriteLine("Contador da lista: " + list.Count);
-
-                string s1 = list.Find(x => x[0] == 'A');/*função lambda*/
-                Console.WriteLine("Primeira letra 'A': " + s1);
-
-                string s2 = list.FindLast(x => x[0] == 'A');/*função lambda*/
-                Console.WriteLine("Ultima letra 'A': " + s1);
-
-                int pos1 = list.FindIndex(x => x[0] == 'A');
-                Console.WriteLine("Primeira posição que começa com 'A': " + s1);
-
-                int pos2 = list.FindLastIndex(x => x[0] == 'A');
-                Console.WriteLine("Ultima posição que começa com 'A': " + s1);
-
-                List<string> list2 = list.FindAll(x => x.Length == 5);
-                Console.WriteLine("------------");
-                foreach(string obj in list2) {
-                    Console.WriteLine(obj);
-                }
-
-                list.RemoveRange(2, 2);
+            for (int i = 0; i < quant; i++) {
+                Console.WriteLine("Empregado #"+ i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Salario: R$ ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                lista.Add(new Empregado(id, name, salary));
                 Console.WriteLine();
-                Console.WriteLine("removeRange------------");
-                foreach (string obj in list) {
-                    Console.WriteLine(obj);
-                }
+            }
 
-                list.Remove("Alex");
-                Console.WriteLine();
-                Console.WriteLine("remove------------");
-                foreach (string obj in list) {
-                    Console.WriteLine(obj);
-                }
+            Console.Write("Insira o ID do empregado que terá o reajuste de salario: ");
+            int ProcId = int.Parse(Console.ReadLine());
 
-                list.RemoveAt(2);
-                Console.WriteLine();
-                Console.WriteLine("removeAT------------");
-                foreach (string obj in list) {
-                    Console.WriteLine(obj);
-                }
+            Empregado emp = lista.Find(x => x.Id == ProcId);
+            if(emp != null) {
+                Console.Write("Digite a porcentagem de reajuste: ");
+                double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.AcrescentarSalario(percent);
+            } else {
+                Console.WriteLine("ID não encontrado!");
+            }
 
-                list.RemoveAll(x => x[0] == 'M');
-                Console.WriteLine();
-                Console.WriteLine("removeall------------");
-                foreach (string obj in list) {
-                    Console.WriteLine(obj);
-                }
+            Console.WriteLine();
+            Console.WriteLine("Salario de empregado ajustado!");
+            foreach(Empregado obj in lista) {
+                Console.WriteLine(obj);
+            }
 
         }
     }
