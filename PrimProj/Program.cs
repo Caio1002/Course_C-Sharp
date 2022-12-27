7521f06b7b981fe63d47c86417b44a87caeba594
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-12-26 21:22:49 -0300

    Intro Matriz

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -7,41 +7,12 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            Console.Write("Quantidade de empregados a serem registrados: ");
-            int quant = int.Parse(Console.ReadLine());
-            Console.WriteLine();
+            double[,] mat = new double[2, 3];
 
-            List<Empregado> lista = new List<Empregado>();
-
-            for (int i = 0; i < quant; i++) {
-                Console.WriteLine("Empregado #"+ i + ":");
-                Console.Write("Id: ");
-                int id = int.Parse(Console.ReadLine());
-                Console.Write("Nome: ");
-                string name = Console.ReadLine();
-                Console.Write("Salario: R$ ");
-                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                lista.Add(new Empregado(id, name, salary));
-                Console.WriteLine();
-            }
-
-            Console.Write("Insira o ID do empregado que terá o reajuste de salario: ");
-            int ProcId = int.Parse(Console.ReadLine());
-
-            Empregado emp = lista.Find(x => x.Id == ProcId);
-            if(emp != null) {
-                Console.Write("Digite a porcentagem de reajuste: ");
-                double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                emp.AcrescentarSalario(percent);
-            } else {
-                Console.WriteLine("ID não encontrado!");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Salario de empregado ajustado!");
-            foreach(Empregado obj in lista) {
-                Console.WriteLine(obj);
-            }
+            Console.WriteLine(mat.Length);//quantidade de elementos no total
+            Console.WriteLine(mat.Rank);//linhas da matriz
+            Console.WriteLine(mat.GetLength(0));//quantidade de elementos da primeira dimensão
+            Console.WriteLine(mat.GetLength(1));//quantidade de elementos da segunda dimensão
 
         }
     }
