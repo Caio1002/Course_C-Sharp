3f186be43e7a7e70b7ab6640ec021e1bf0d42615
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-12-28 22:01:36 -0300

    Exercicio matriz

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -7,66 +7,35 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            List<string> list = new List<string>();
+            Console.Write("Digite a dimensão da matriz: ");
+            int n = int.Parse(Console.ReadLine());
 
-            list.Add("Maria");
-            list.Add("Alex");
-            list.Add("Bob");
-            list.Add("Ana");
-            list.Add("Carlos");
-            list.Add("João");
-            list.Insert(2, "Marco");
+            int[,] mat = new int[n, n];
 
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
+            for (int i = 0; i < n; i++) {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+                }
             }
-            Console.WriteLine("Contador da lista: " + list.Count);
-
-            string s1 = list.Find(x => x[0] == 'A'); //função lambda
-            Console.WriteLine("Primeira letra 'A': " + s1);
-
-            string s2 = list.FindLast(x => x[0] == 'A'); //função lambda
-            Console.WriteLine("Ultima letra 'A': " + s1);
-
-            int pos1 = list.FindIndex(x => x[0] == 'A');
-            Console.WriteLine("Primeira posição que começa com 'A': " + s1);
-
-            int pos2 = list.FindLastIndex(x => x[0] == 'A');
-            Console.WriteLine("Ultima posição que começa com 'A': " + s1);
-
-            List<string> list2 = list.FindAll(x => x.Length == 5);
-            Console.WriteLine("------------");
-            foreach (string obj in list2) {
-                Console.WriteLine(obj);
-            }
-
-            list.RemoveRange(2, 2);
             Console.WriteLine();
-            Console.WriteLine("removeRange------------");
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
-            }
 
-            list.Remove("Alex");
-            Console.WriteLine();
-            Console.WriteLine("remove------------");
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
+            Console.WriteLine("Diagonal principal: ");
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine(mat[i,i] + " ");
             }
+            Console.WriteLine();
 
-            list.RemoveAt(2);
-            Console.WriteLine();
-            Console.WriteLine("removeAT------------");
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
+            Console.WriteLine("Quantidade de numeros negativos:");
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(mat[i, j] < 0) {
+                        count++;
+                    }
+                }
             }
-
-            list.RemoveAll(x => x[0] == 'M');
-            Console.WriteLine();
-            Console.WriteLine("removeall------------");
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
-            }
+            Console.WriteLine(count);
 
         }
     }
