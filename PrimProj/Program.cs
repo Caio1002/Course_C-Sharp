afd0368681b4b48f05510711bffc51c5b0412d19
Caio Rodrigues <caiodaluz2002@gmail.com>
2022-12-28 22:57:37 -0300

    Atividade Matriz

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -7,36 +7,44 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            Console.Write("Digite a dimensão da matriz: ");
+            Console.Write("Digite a quantidade de linhas da matriz: ");
             int n = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de colunas da matriz: ");
+            int m = int.Parse(Console.ReadLine());
 
-            int[,] mat = new int[n, n];
+            int[,] mat = new int[n, m];
 
             for (int i = 0; i < n; i++) {
                 string[] values = Console.ReadLine().Split(' ');
-                for (int j = 0; j < n; j++) {
+                for (int j = 0; j < m; j++) {
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
             Console.WriteLine();
 
-            Console.WriteLine("Diagonal principal: ");
+            Console.Write("Digite um numero para busca: ");
+            int x = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++) {
-                Console.WriteLine(mat[i,i] + " ");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Quantidade de numeros negativos:");
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(mat[i, j] < 0) {
-                        count++;
+                for (int j = 0; j < m; j++) {
+                    if (mat[i, j] == x) {
+                        Console.WriteLine("Posição: " + i + "," + j + ":");
+                        if (j > 0) {
+                            Console.WriteLine("Esquerda: " + mat[i, j - 1]);
+                        }
+                        if (i > 0) {
+                            Console.WriteLine("Em cima: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1) {
+                            Console.WriteLine("Direita: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1) {
+                            Console.WriteLine("Em baixo: " + mat[i + 1, j]);
+                        }
                     }
                 }
             }
-            Console.WriteLine(count);
-
+            Console.WriteLine();
         }
+
     }
 }
