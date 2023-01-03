75e35b1e32979f08521fe33950abfa90000c93cd
Caio Rodrigues <caiodaluz2002@gmail.com>
2023-01-03 00:16:24 -0300

    Switch-case

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -7,13 +7,37 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            var x = 10;
-            var y = 20.0;
-            var z = "Maria";
+            int x = int.Parse(Console.ReadLine());
+            string day;
 
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            switch (x) {
+                case 1:
+                    day = "Domingo";
+                    break;
+                case 2:
+                    day = "Segunda-feira";
+                    break;
+                case 3:
+                    day = "Terça-feira";
+                    break;
+                case 4:
+                    day = "Quarta-feira";
+                    break;
+                case 5:
+                    day = "Quinta-feira";
+                    break;
+                case 6:
+                    day = "Sexta-feira";
+                    break;
+                case 7:
+                    day = "Sabado";
+                    break;
+                default:
+                    day = "Inválido";
+                    break;
+            }
+
+            Console.WriteLine(day);
 
         }
     }
