e89292b343684f019db85ccc865441ea28c0307d
Caio Rodrigues <caiodaluz2002@gmail.com>
2023-01-03 20:01:23 -0300

    Condição ternária

---
+++ PrimProj/Anotacoes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimProj {
+    internal class Anotacoes {
+
+        /*
+        VAR
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        */
+
+        /*
+        Switch Case
+        int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x) {
+                case 1:
+                    day = "Domingo";
+                    break;
+                case 2:
+                    day = "Segunda-feira";
+                    break;
+                case 3:
+                    day = "Terça-feira";
+                    break;
+                case 4:
+                    day = "Quarta-feira";
+                    break;
+                case 5:
+                    day = "Quinta-feira";
+                    break;
+                case 6:
+                    day = "Sexta-feira";
+                    break;
+                case 7:
+                    day = "Sabado";
+                    break;
+                default:
+                    day = "Inválido";
+                    break;
+            }
+
+            Console.WriteLine(day);
+        */
+
+        /*
+        Condição ternária
+        */
+
+    }
+}
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -7,37 +7,12 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            int x = int.Parse(Console.ReadLine());
-            string day;
+            /*(CONDIÇÃO) ? VALOR_SE_VERDADEIRO : VALOR_SE_FALSO*/
 
-            switch (x) {
-                case 1:
-                    day = "Domingo";
-                    break;
-                case 2:
-                    day = "Segunda-feira";
-                    break;
-                case 3:
-                    day = "Terça-feira";
-                    break;
-                case 4:
-                    day = "Quarta-feira";
-                    break;
-                case 5:
-                    day = "Quinta-feira";
-                    break;
-                case 6:
-                    day = "Sexta-feira";
-                    break;
-                case 7:
-                    day = "Sabado";
-                    break;
-                default:
-                    day = "Inválido";
-                    break;
-            }
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
 
-            Console.WriteLine(day);
+            Console.WriteLine(desconto);
 
         }
     }
