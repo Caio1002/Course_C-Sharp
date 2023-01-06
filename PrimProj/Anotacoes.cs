4b714c015b42b1e80761bdb4e937f5a2dc64c1a9
Caio Rodrigues <caiodaluz2002@gmail.com>
2023-01-05 23:14:47 -0300

    Funções string

--- PrimProj/Anotacoes.cs
+++ PrimProj/Anotacoes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,6 +56,12 @@ namespace PrimProj {
 
         /*
         Condição ternária
+        /*(CONDIÇÃO) ? VALOR_SE_VERDADEIRO : VALOR_SE_FALSO*/
+        /*
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        Console.WriteLine(desconto);
         */
 
     }
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -7,12 +7,48 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            /*(CONDIÇÃO) ? VALOR_SE_VERDADEIRO : VALOR_SE_FALSO*/
+            string original = "abcde FGHIJ ABC abc DEFG  ";
+            Console.WriteLine("Original: " + original + " - ");
 
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            string s1 = original.ToUpper();
+            Console.WriteLine("Maiuscula: " + s1 + " - ");
 
-            Console.WriteLine(desconto);
+            string s2 = original.ToLower();
+            Console.WriteLine("Minuscula: " + s2 + " - ");
+
+            //apagar espaços em branco
+            string s3 = original.Trim();
+            Console.WriteLine("Sem espaço: " + s3 + "- ");
+
+            //procura string
+            int n1 = original.IndexOf("bc");
+            Console.WriteLine("bc encontrado " + n1 + " vezes.");
+
+            int n2 = original.LastIndexOf("bc");
+            Console.WriteLine("Localização do ultimo registro 'bc' na string: " + n2);
+
+            //cortar string
+            string s4 = original.Substring(3);
+            Console.WriteLine("Substring: " + s4);
+
+            //cortar string
+            string s5 = original.Substring(3, 5);
+            Console.WriteLine("Substring inicio e fim: " + s5);
+
+            //substituir string
+            string n6 = original.Replace('a', 'x');
+            Console.WriteLine("Substituição: " + n6);
+
+            string n7 = original.Replace("abc", "xy");
+            Console.WriteLine("Substituição: " + n7);
+
+            //testar string vazia
+            bool b1 = String.IsNullOrEmpty(original);
+            Console.WriteLine("Nulo? " + b1);
+
+            //testar espaço em branco
+            bool b2 = String.IsNullOrWhiteSpace(original);
+            Console.WriteLine("Testar espaço em branco: " + b2);
 
         }
     }
