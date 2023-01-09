4ffa26644c37e00673d5852cdcb3680bb169cfa0
Caio Rodrigues <caiodaluz2002@gmail.com>
2023-01-08 22:58:43 -0300

    TimesSpan

--- PrimProj/Anotacoes.cs
+++ PrimProj/Anotacoes.cs
@@ -109,5 +109,29 @@ namespace PrimProj {
             Console.WriteLine("Testar espaço em branco: " + b2);
         */
 
+        /*
+        DateTime d1 = DateTime.Now;//marcar hora atual
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);//marcar hora em ticks desde 01 Jan
+
+            DateTime d2 = new DateTime(2019, 10, 14, 11, 02, 55);
+            Console.WriteLine(d2);
+
+            DateTime d3 = new DateTime(2019, 10, 14, 11, 02, 55, 500);
+            Console.WriteLine(d3);
+
+            DateTime d4 = DateTime.Today;
+            Console.WriteLine(d4);
+
+            DateTime d5 = DateTime.Parse("15/08/2020");//br
+            Console.WriteLine(d5);
+
+            DateTime d6 = DateTime.Parse("2000-07-10 13:01:57");
+            Console.WriteLine(d6);
+
+            DateTime d7 = DateTime.ParseExact("2002-09-19", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            Console.WriteLine(d7);
+        */
+
     }
 }
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -8,27 +8,18 @@ namespace PrimProj {
     class Program {
         static void Main(string[] args) {
 
-            DateTime d1 = DateTime.Now;//marcar hora atual
-            Console.WriteLine(d1);
-            Console.WriteLine(d1.Ticks);//marcar hora em ticks desde 01 Jan
+            //Duração de tempo
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            Console.WriteLine(t1);
 
-            DateTime d2 = new DateTime(2019, 10, 14, 11, 02, 55);
-            Console.WriteLine(d2);
+            TimeSpan t2 = new TimeSpan(900000000L);
+            Console.WriteLine(t2);
 
-            DateTime d3 = new DateTime(2019, 10, 14, 11, 02, 55, 500);
-            Console.WriteLine(d3);
+            TimeSpan t3 = TimeSpan.FromDays(1.5);
+            Console.WriteLine(t3);
 
-            DateTime d4 = DateTime.Today;
-            Console.WriteLine(d4);
-
-            DateTime d5 = DateTime.Parse("15/08/2020");//br
-            Console.WriteLine(d5);
-
-            DateTime d6 = DateTime.Parse("2000-07-10 13:01:57");
-            Console.WriteLine(d6);
-
-            DateTime d7 = DateTime.ParseExact("2002-09-19", "yyyy-MM-dd", CultureInfo.InvariantCulture);
-            Console.WriteLine(d7);
+            TimeSpan t4 = TimeSpan.FromHours(1.7);
+            Console.WriteLine(t4);
 
         }
     }
