2be0c0489225d1c5027d3bbf50770983e56dbb5a
Caio Rodrigues <caiodaluz2002@gmail.com>
2023-01-09 21:02:23 -0300

    Enum

--- PrimProj/Anotacoes.cs
+++ PrimProj/Anotacoes.cs
@@ -153,5 +153,20 @@ namespace PrimProj {
             Console.WriteLine(x);
         */
 
+        /*
+        //DateTime
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d2 kind: " + d2.Kind);
+            Console.WriteLine("d3 to local: " + d3.ToLocalTime());
+            Console.WriteLine("d4 to utc: " + d3.ToUniversalTime());
+
+            //ISO 8601
+            // yyyy-MM-ddTHH:mm:ssz 
+        */
+
     }
 }
--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -1,21 +1,33 @@
-﻿using System;
+﻿using PrimProj.Entities;
+using PrimProj.Entities.Enums;
+using System;
 using System.Globalization;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Data;
 
-namespace PrimProj {
-    class Program {
-        static void Main(string[] args) {
+namespace PrimProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
-            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
-            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
 
-            Console.WriteLine("d1: " + d1);
-            Console.WriteLine("d2 kind: " + d2.Kind);
-            Console.WriteLine("d3 to local: " + d3.ToLocalTime());
-            Console.WriteLine("d4 to utc: " + d3.ToUniversalTime());
+            Console.WriteLine(order);
+
+            String  txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
 
         }
     }
