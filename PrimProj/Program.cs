5aeb2d24fdba5dac0aaa697a889b005a4fb035b1
Caio Rodrigues <caiodaluz2002@gmail.com>
2023-02-01 22:12:59 -0300

    Try-catch Exception

--- PrimProj/Program.cs
+++ PrimProj/Program.cs
@@ -5,6 +5,8 @@ using System.Globalization;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Data;
+using System.IO;
+using PrimProj.Entities.Exceptions;
 
 namespace PrimProj
 {
@@ -14,14 +16,28 @@ namespace PrimProj
         {
             try
             {
-                int n1 = int.Parse(Console.ReadLine());
-                int n2 = int.Parse(Console.ReadLine());
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date: (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date: (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
 
-                int result = n1 / n2;
-                Console.WriteLine(result);
-            } catch(DivideByZeroException)
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            } catch (DomainException e)
             {
-                Console.WriteLine("Division by zero is not allowed");
+                Console.WriteLine("Error in reservation: " + e.Message);
             }
 
         }
